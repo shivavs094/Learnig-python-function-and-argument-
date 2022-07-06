d830d2cad935c0cfd2b57d19a051128196070f64
Shiva <108185580+shivavs094@users.noreply.github.com>
2022-07-06 19:54:23 +0530

    Add files via upload

---
+++ polymophism_Ducktype.py
@@ -0,0 +1,22 @@
+class pycharm:
+    def exicute(self):
+        print("Writte")
+        print("compaile")
+
+class mycharm:
+
+    def exicute (self):
+        print("spell check")
+        print("conversion check")
+        print("compile")
+        print("Running")
+
+
+
+class laptop:
+    def code(self,ide):
+        ide.exicute()
+
+ide= pycharm()
+lap1=laptop()
+lap1.code(ide)
